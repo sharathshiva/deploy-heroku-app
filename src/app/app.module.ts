9250de8453381a7887080775c36d51d7aa8abe50
unknown <sharathshiva2000@gmail.com>
2022-03-28 19:44:43 +0530

    deploy_heroku_application_modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 import { AppNavigationComponent } from './component/app-navigation/app-navigation.component';
 import { AppDetailsComponent } from './component/app-right-side-details/app-right-side-details.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppLeftSideDataComponent } from './component/app-left-side-data/app-left-side-data.component';
 import { AppLeftSideHeaderComponent } from './component/app-left-side-header/app-left-side-header.component';
+import { RightSideHeaderComponent } from './component/app-right-side-header/right-side-header.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularSplitModule } from 'angular-split';
+import { HttpClientModule } from '@angular/common/http';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatCheckboxModule} from '@angular/material/checkbox';
@@ -17,8 +18,6 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatCardModule} from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
-import { RightSideHeaderComponent } from './component/app-right-side-header/right-side-header.component';
-
 
 @NgModule({
   declarations: [
@@ -33,7 +32,6 @@ import { RightSideHeaderComponent } from './component/app-right-side-header/righ
     BrowserModule,
     AngularSplitModule,
     HttpClientModule,
-    BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
     MatMenuModule,
